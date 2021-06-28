00c5c418b3b5271fabd752354be2142e7b1d4cea
Swarali735-ai <83956995+Swarali735-ai@users.noreply.github.com>
2021-06-28 16:40:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0
+noseY=0
+
+function preload() {
+clown_nose = loadImage('https://i.postimg.cc/MH5WyV4n/pngtree-vector-cartoon-gentleman-uncle-blue-beard-element-png-image-4602845-removebg-preview.png');
+}
+
+
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x -35;
+        noseY = results[0].pose.nose.y -20;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 100, 100);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
